415b17fee6c7c26f9ecd901967f0ec3eb99e483c
Semenov <Semenov@mail.ru>
2024-01-07 14:19:24 +0400

    68 - разработка Router

--- src/app/lib/router/Router.ts
+++ src/app/lib/router/Router.ts
@@ -9,17 +9,43 @@ export class Router {
     }
     
     init(){
-        window.addEventListener('load', () => {
+        window.addEventListener('click', (event: MouseEvent) => {
+            const target = event.target as HTMLElement;
+            
+            if(target.tagName != "A"){
+                return;
+            }
+
+            console.log("click tagName == A");
+            event.preventDefault();
+            const href = target.getAttribute("href");
+            if(!href) {
+                return;
+            }
+
+            console.log("click tagName == A and href");
+            history.pushState(null, null, href);
             this.loadRoute();
-        })
+        });
+        
+        window.addEventListener('load', () => {
+            console.log("window load");
+            this.loadRoute();
+        }, { once: true })
 
         window.addEventListener('popstate', () => {
+            console.log("window popstate"); 
             this.loadRoute();
         });
     }
 
     async loadRoute() {
         const route = this.routes.find(r => r.path === location.pathname);
-        document.querySelector('#app').innerHTML = await route.page.render();
+        if(route) {
+            document.querySelector('#app').innerHTML = await route.page.render();
+        }else {
+            // TODO требуется доработка с выводом 404 ошибки
+            console.error("требуется доработка с выводом 404");
+        }
     }
 }
--- src/page/Home/index.ts
+++ src/page/Home/index.ts
@@ -3,7 +3,7 @@ import type { PageType } from "src/shared";
 const HomePage = (): PageType => {
     
     function render() {
-        return `<a href="/sql-generator">sql</a><div>Home Page</div>`;
+        return `<div>Home Page</div>`;
     }
     return { render }
 };
