7dce4b00d7a19adceae8bc417677dc823d9f887e
Semenov <Semenov@mail.ru>
2024-02-16 20:36:09 +0400

    очистка плохо решения на классах

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,13 +1,1 @@
-import { Router } from "@app/lib/router/Router";
-import { Route } from "@app/lib/router/Route";
-import { HomePage } from "@page/Home/index";
-import { SqlGeneratorPage } from "@page/SqlGenerator/index";
-import { LayoutMainComponent } from "@/widgets/LayoutMain";
-
-const layoutMain = new LayoutMainComponent();
-new Router(
-    [
-        new Route("/", new HomePage(), layoutMain),
-        new Route("/sql-generator", new SqlGeneratorPage(), layoutMain),
-    ]
-)
+console.log(2);
--- src/widgets/LayoutMain/types/LayoutMainType.ts
+++
@@ -1,6 +0,0 @@
-import { ComponentType, PageType } from "@/shared/types";
-
-export interface LayoutMainType extends ComponentType {
-    render: (page?: PageType) => string;
-    renderSlot: (page?: PageType) => string;
-}
